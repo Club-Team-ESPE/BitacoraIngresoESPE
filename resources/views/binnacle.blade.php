b1f93f6482101f7c1a106a3803d56860e959e079
BryanCruz94 <bscruz@espe.edu.ec>
2023-08-07 00:09:55 -0500

    Interfaz Novedades

--- resources/views/binnacle.blade.php
+++ resources/views/binnacle.blade.php
@@ -5,16 +5,122 @@
 @section('content_header')
     <div class="row justify-content-center align-item-center">
         <div class="col text-center">
-        <h1>NOVEDADES</h1>
+            <h1>BITÁCORA NOVEDADES UFA-ESPE "SANTO DOMINGO"</h1>
         </div>
     </div>
-    
+    <div class="row h-100 mt-1">
+        <div class="col-10 offset-1 d-flex align-items-center justify-content-center">
+            <button class="btn btn-lg btn-success" mr-5" data-toggle="modal" data-target="#modalRegistro">REGISTRAR
+                NOVEDAD</button>
+        </div>
+    </div>
+
 @stop
 
+
 @section('content')
+    {{-- ESPACIO DE BOTÓN PARA REGISTRAR NOVEDADES --}}
+
     <div class="row">
+
         <div class="col ">
-            AQUÍ VA LA TABLA DE MOVIMIENTO DE NOVEDADES
+            {{-- TABLA SUPERIOR --}}
+            <div class="card">
+
+                <div class="card-header">
+                    <h1 class="card-title">REGISTRO DIARIO</h1>
+                    <div class="card-tools">
+                    </div>
+                </div>
+
+                <div class="card-body table-responsive p-0">
+                    <table id="tablaUno" class="table table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th class="text-center">ORD</th>
+                                <th class="text-center">NOVEDADES</th>
+                                <th class="text-center">FECHA/HORA</th>
+                                <th class="text-center">GUARDIA TURNO</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach ($registros1 as $item)
+                        <tr>
+                            <td>
+                                {{ $item->PLACA }}
+                            </td>
+                            <td style="max-width: 800px; min-width: 200px;
+                            white-space: normal;">
+                                {{ $item->DESCRIPCION }}
+                            </td>
+                            <td>
+                                {{ $item->REGISTRO }}
+                            </td>
+                            <td>
+                                {{ $item->FECHAHORASALIDA }}
+                            </td>
+                        </tr>
+                    @endforeach --}}
+
+                            {{-- MEDIANTE UN CICLO FOR CREAR 4 FILAS CON 4 COLUMNAS CADA UNA --}}
+                            @for ($i = 0; $i < 5; $i++)
+                                <tr>
+                                    <td class="text-center">
+                                        {{ $i + 1 }}
+                                    </td>
+                                    <td
+                                        style="max-width: 800px; min-width: 200px;
+                            white-space: normal;">
+                                        NOVEDADES
+                                    </td>
+                                    <td class="text-center">
+                                        -
+                                    </td>
+                                    <td class="text-center">
+                                        -
+                                    </td>
+                                </tr>
+                            @endfor
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    {{-- MODAL PARA REGISTRAR UNA NUEVA NOVEDAD --}}
+    <div class="modal" tabindex="-1" id="modalRegistro">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">NUEVA NOVEDAD</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="#" method="POST">
+                    @csrf
+                    <div class="modal-body">
+
+                        <div id="modificarRegistro" class="row mt-3" style="">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="observacion">Detalle la novedad</label>
+                                    <textarea name="observacion" id="observacion" class="form-control" rows="3" ></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
+                        <button type="submit" id="btnIngreso" style="" class="btn btn-success">GUARDAR</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
@@ -25,5 +131,17 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+
+
+        $("#tablaUno").dataTable({
+            "paging": true,
+            "ordering": false,
+            "language": {
+                url: 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json'
+            },
+            "searching": true
+        });
+    </script>
 @stop
