29941a177dc1347775faa7d3f74b07d917273b9e
BryanCruz94 <bscruz@espe.edu.ec>
2023-11-29 11:47:23 -0500

    feat(routes): Proteccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -23,36 +23,36 @@ Route::get('prueba', function () {
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
 Route::get('/index1', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
 // ****************** BITÁCORA NOVEDADES ******************
 //RUTA PARA MOSTRAR NOVEDADES
-Route::get('/binnacle', [NoveltyController::class,'index'])->name("binnacle.index");
+Route::get('/binnacle', [NoveltyController::class,'index'])->middleware('auth')->name("binnacle.index");
 //CREAR NUEVA NOVEDAD
-Route::post('/binnacle/newBinnacle', [NoveltyController::class,'store'])->name("binacle.store");
+Route::post('/binnacle/newBinnacle', [NoveltyController::class,'store'])->middleware('auth')->name("binacle.store");
 
 // ****************** BITÁCORA CONSIGNAS ******************
 //RUTA PARA MOSTRAR CONSIGNAS PENDIENTES
-Route::get('/pendings', [PendingTaskController::class,'index'])->name("pendding.index");
+Route::get('/pendings', [PendingTaskController::class,'index'])->middleware('auth')->name("pendding.index");
 //CREAR NUEVA CONSIGNA
-Route::post('/pendings/newPending', [PendingTaskController::class,'store'])->name("pendding.store");
+Route::post('/pendings/newPending', [PendingTaskController::class,'store'])->middleware('auth')->name("pendding.store");
 
 
 Route::get('/vehicles', function () {
     return view('vehicles');
-});
+})->middleware('auth');
 
 Route::get('/reports', function () {
     return view('reports');
-});
+})->middleware('auth');
 
 
 
 
 //Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('home');
